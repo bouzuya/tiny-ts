761810c9598488a78f857e4084975577a6d4cba2
bouzuya <m@bouzuya.net>
2025-05-09 22:51:48 +0900

    Add some tokens for basic

---
+++ src/tiny_ts/basic.rs
@@ -0,0 +1,10 @@
+// mod parse;
+// mod term;
+mod token;
+// mod typ;
+// mod typecheck;
+
+// pub use self::parse::parse;
+// pub use self::term::Term;
+// pub use self::typ::Type;
+// pub use self::typecheck::typecheck;
---
+++ src/tiny_ts/basic/token.rs
@@ -0,0 +1,103 @@
+#[derive(Debug, PartialEq, logos::Logos)]
+#[logos(skip r"\s+")]
+pub enum Token {
+    #[token("false")]
+    False,
+    #[token("true")]
+    True,
+    #[regex("0|[1-9][0-9]{0,}", |lexer| lexer.slice().parse::<u8>().ok())]
+    Integer(u8),
+    #[token("+")]
+    Plus,
+    #[token("?")]
+    Quest,
+    #[token(":")]
+    Colon,
+    #[regex("[a-zA-Z_][a-zA-Z0-9_]{0,}", |lexer| lexer.slice().to_string())]
+    Ident(String),
+    #[token(";")]
+    Semicolon,
+    #[token("const")]
+    Const,
+    #[token("=")]
+    Equals,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_arith() {
+        let mut lexer = <Token as logos::Logos>::lexer("true ? 0 : 1 + 2");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::True)));
+        assert_eq!(lexer.span(), 0..4);
+        assert_eq!(lexer.slice(), "true");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Quest)));
+        assert_eq!(lexer.span(), 5..6);
+        assert_eq!(lexer.slice(), "?");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Integer(0))));
+        assert_eq!(lexer.span(), 7..8);
+        assert_eq!(lexer.slice(), "0");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Colon)));
+        assert_eq!(lexer.span(), 9..10);
+        assert_eq!(lexer.slice(), ":");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Integer(1))));
+        assert_eq!(lexer.span(), 11..12);
+        assert_eq!(lexer.slice(), "1");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Plus)));
+        assert_eq!(lexer.span(), 13..14);
+        assert_eq!(lexer.slice(), "+");
+
+        assert_eq!(lexer.next(), Some(Ok(Token::Integer(2))));
+        assert_eq!(lexer.span(), 15..16);
+        assert_eq!(lexer.slice(), "2");
+
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn test_basic() {
+        let mut lexer = <Token as logos::Logos>::lexer("x");
+        assert_eq!(lexer.next(), Some(Ok(Token::Ident("x".to_owned()))));
+        assert_eq!(lexer.span(), 0..1);
+        assert_eq!(lexer.slice(), "x");
+        assert_eq!(lexer.next(), None);
+
+        let mut lexer = <Token as logos::Logos>::lexer("f");
+        assert_eq!(lexer.next(), Some(Ok(Token::Ident("f".to_owned()))));
+        assert_eq!(lexer.span(), 0..1);
+        assert_eq!(lexer.slice(), "f");
+        assert_eq!(lexer.next(), None);
+
+        // TODO: (x: number) => x
+        // TODO: f(1)
+
+        let mut lexer = <Token as logos::Logos>::lexer("const x = 1; x");
+        assert_eq!(lexer.next(), Some(Ok(Token::Const)));
+        assert_eq!(lexer.span(), 0..5);
+        assert_eq!(lexer.slice(), "const");
+        assert_eq!(lexer.next(), Some(Ok(Token::Ident("x".to_owned()))));
+        assert_eq!(lexer.span(), 6..7);
+        assert_eq!(lexer.slice(), "x");
+        assert_eq!(lexer.next(), Some(Ok(Token::Equals)));
+        assert_eq!(lexer.span(), 8..9);
+        assert_eq!(lexer.slice(), "=");
+        assert_eq!(lexer.next(), Some(Ok(Token::Integer(1))));
+        assert_eq!(lexer.span(), 10..11);
+        assert_eq!(lexer.slice(), "1");
+        assert_eq!(lexer.next(), Some(Ok(Token::Semicolon)));
+        assert_eq!(lexer.span(), 11..12);
+        assert_eq!(lexer.slice(), ";");
+        assert_eq!(lexer.next(), Some(Ok(Token::Ident("x".to_owned()))));
+        assert_eq!(lexer.span(), 13..14);
+        assert_eq!(lexer.slice(), "x");
+        assert_eq!(lexer.next(), None);
+    }
+}
